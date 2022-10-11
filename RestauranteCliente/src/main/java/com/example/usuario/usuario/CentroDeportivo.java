6b47a8a50c88750c204cf2be741a7762a5a3cf22
Constanza <cmira@correo.um.edu.uy>
2022-10-11 18:25:25 -0300

    cambios2.0

---
+++ RestauranteCliente/src/main/java/com/example/usuario/usuario/CentroDeportivo.java
@@ -0,0 +1,62 @@
+package com.example.usuario.usuario;
+
+public class CentroDeportivo {
+
+    private Long rut;
+
+    private Long telefono;
+
+    private String nombre;
+
+    private String direccion;
+
+
+    public CentroDeportivo(Long telefono, String nombre, String direccion, Long rut) {
+        this.telefono = telefono;
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.rut = rut;
+    }
+
+    public CentroDeportivo() {
+
+    }
+
+    public Long getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(Long telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public Long getRut() {return rut;}
+
+    public void setRut(Long rut) {this.rut = rut;}
+
+    @Override
+    public String toString() {
+        return "Centro Deportivo{" +
+                "rut=" + rut +
+                "telefono=" + telefono +
+                ", nombre='" + nombre + '\'' +
+                ", direccion='" + direccion + '\'' +
+                '}';
+    }
+}
---
+++ RestauranteCliente/src/main/java/com/example/usuario/usuario/Empresa.java
@@ -0,0 +1,32 @@
+package com.example.usuario.usuario;
+
+public class Empresa {
+
+    private Long rut;
+
+    private String nombre;
+
+    public Empresa() {
+    }
+
+    public Empresa(String nombre, Long rut) {
+        this.nombre = nombre;
+        this.rut = rut;
+    }
+
+    public String getNombre() {return nombre;}
+
+    public void setNombre(String nombre) {this.nombre = nombre;}
+
+    public Long getRut() {return rut;}
+
+    public void setRut(Long rut) {this.rut = rut;}
+
+    @Override
+    public String toString() {
+        return "Empresa{" +
+                "rut=" + rut +
+                "nombre='" + nombre + '\'' +
+                '}';
+    }
+}
