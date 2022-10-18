6a3265bfeca30ab899ff58be6dc6c6436cb465db
valemailhos <vmailhos@correo.um.edu.uy>
2022-10-18 18:27:14 -0300

    Cambio_LogIn por el viejo

--- RestauranteCliente/src/main/java/com/example/usuario/usuario/LogInController.java
+++ RestauranteCliente/src/main/java/com/example/usuario/usuario/LogInController.java
@@ -1,10 +1,6 @@
 package com.example.usuario.usuario;
 
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.fasterxml.jackson.databind.type.ReferenceType;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -17,26 +13,15 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
-import kong.unirest.HttpResponse;
-import kong.unirest.JsonNode;
-import kong.unirest.Unirest;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.ResourceBundle;
 
 public class LogInController implements Initializable {
     private Stage stage;
 
     private Scene scene;
-
-    List<Usuarios> usuarios = new ArrayList<>();
-
-    String email_;
-
-    String contraseña_;
 
     @FXML
     private AnchorPane LogInButton;
@@ -62,54 +47,28 @@ public class LogInController implements Initializable {
 
     @FXML
     void LogInClickedButton(ActionEvent event) throws IOException {
-        if(!txt_contraseña.getText().isEmpty() && !txt_email.getText().isEmpty()) {
+        if (txt_email.getText().equals("admin") && txt_contraseña.getText().equals("admin")) {
+            Parent root = FXMLLoader.load(getClass().getResource("/com/example/usuario/usuario/Opciones1-view.fxml"));
+            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        }
 
-            email_ = txt_email.getText();
-            contraseña_ = txt_contraseña.getText();
-            String json = "";
-            ObjectMapper mapper = new ObjectMapper();
-            ObjectNode rest = mapper.createObjectNode();
-            rest.put(email_,"email");
-            rest.put(contraseña_, "contraseña");
-            json = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(rest);
+        if (txt_email.getText().equals("centro") && txt_contraseña.getText().equals("centro")) {
+            Parent root = FXMLLoader.load(getClass().getResource("/com/example/usuario/usuario/CrearActividades-view.fxml"));
+            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        }
 
-
-            HttpResponse<JsonNode> apiResponse = Unirest.post("http://localhost:8080/api/v1/gimnasio/usuarios")
-                    .header("Content-Type", "application/json").body(json).asJson();
-            int response = apiResponse.getStatus();
-
-            if (response != 400) {
-                Usuarios listUser = mapper.readValue(apiResponse.getBody().toString(), new TypeReference<Usuarios>() {});
-                if (listUser.getTipoUsuario().equals("superAdmin")) {
-                    Parent root = FXMLLoader.load(getClass().getResource("/com/example/usuario/usuario/Opciones1-view.fxml"));
-                    stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-                    scene = new Scene(root);
-                    stage.setScene(scene);
-                    stage.show();
-                }
-
-                if (listUser.getTipoUsuario().equals("adminCentroDeportivo")) {
-                    Parent root = FXMLLoader.load(getClass().getResource("/com/example/usuario/usuario/Actividades/CrearActividades-view.fxml"));
-                    stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-                    scene = new Scene(root);
-                    stage.setScene(scene);
-                    stage.show();
-                }
-
-                //falta adminEmpresa
-
-                if (usuarios.get(response).getTipoUsuario().equals("usuarioEmpleado")) {
-                    Parent root = FXMLLoader.load(getClass().getResource("/com/example/usuario/usuario/Actividades/Actividades-view.fxml"));
-                    stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-                    scene = new Scene(root);
-                    stage.setScene(scene);
-                    stage.show();
-                }
-            } else {
-                System.out.println("Usuario Incorrecto");
-            }
-        }else{
-            System.out.println("Ingrese todos los datos solicitados");
+        if (txt_email.getText().equals("usuario") && txt_contraseña.getText().equals("usuario")) {
+            Parent root = FXMLLoader.load(getClass().getResource("/com/example/usuario/usuario/Actividades-view.fxml"));
+            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
         }
     }
     @FXML
