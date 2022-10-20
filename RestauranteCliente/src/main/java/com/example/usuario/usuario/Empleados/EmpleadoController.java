6a8c05a42c1e8e6fab566cb02532a1c25f568dbb
ValentinAltamirano1 <valtamirano@correo.um.edu.uy>
2022-10-20 13:57:43 -0300

    cambios 3.0

--- RestauranteCliente/src/main/java/com/example/usuario/usuario/Empleados/EmpleadoController.java
+++ RestauranteCliente/src/main/java/com/example/usuario/usuario/Empleados/EmpleadoController.java
@@ -1,5 +1,13 @@
 package com.example.usuario.usuario.Empleados;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -8,6 +16,7 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
@@ -20,6 +29,15 @@ import java.util.ResourceBundle;
 public class EmpleadoController implements Initializable {
     Stage stage;
     Scene scene;
+
+    String nombre_;
+    String pasaporte_;
+    String mail_;
+    int telefono_;
+    String tipo_;
+
+
+
     @FXML
     private Text CrearNuevoEmpleado;
 
@@ -43,19 +61,23 @@ public class EmpleadoController implements Initializable {
 
     @FXML
     private Text telefono;
-
+    @FXML
+    private Text pasaporte;
     @FXML
     private Text titulo;
-
+    @FXML
+    private Text tipo;
     @FXML
     private TextField txt_mail;
 
     @FXML
     private TextField txt_nombre;
-
+    @FXML
+    private TextField txt_pasaporte;
     @FXML
     private TextField txt_telefono;
-
+    @FXML
+    private ChoiceBox<String> txt_tipo;
     @FXML
     void CerrarSesionClickedButton(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("/com/example/usuario/usuario/LogIn-view.fxml"));
@@ -64,6 +86,8 @@ public class EmpleadoController implements Initializable {
         stage.setScene(scene);
         stage.show();
     }
+    ObservableList<String> txt_tipo_list= FXCollections.
+            observableArrayList("Usuario");
 
     @FXML
     void CreadosClickedButton(ActionEvent event) throws IOException {
@@ -76,11 +100,41 @@ public class EmpleadoController implements Initializable {
 
     @FXML
     void CrearClickedButton(ActionEvent event) {
+        if(!txt_nombre.getText().isEmpty() && !txt_mail.getText().isEmpty() && !txt_pasaporte.getText().isEmpty() && !txt_telefono.getText().isEmpty()){
+            try {
+                nombre_ = txt_nombre.getText();
+                mail_ = txt_mail.getText();
+                pasaporte_ = txt_pasaporte.getText();
+                telefono_ = Integer.parseInt(txt_telefono.getText());
+                tipo_ = txt_tipo.getValue();
+                String json = "";
+                try {
+                    ObjectMapper mapper = new ObjectMapper();
+                    ObjectNode rest = mapper.createObjectNode();
+                    rest.put("nombre", nombre_);
+                    rest.put("mail",mail_ );
+                    rest.put("pasaporte", pasaporte_);
+                    rest.put("telefono",telefono_);
+                    rest.put("tipo",tipo_ );
+                    json = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(rest);
+                }catch (Exception ignored) {
+                }
+                try {
+                    HttpResponse<JsonNode> apiResponse = Unirest.post("http://localhost:8080/api/v1/gimnasio/empleado")
+                            .header("Content-Type", "application/json").body(json).asJson();
 
+                }catch (UnirestException ex) {}
+            }catch (NumberFormatException e){}
+            //Empresa nuevaEmpresa = new Empresa(nombre, rut);
+            //empresa.addNewEmpresa(nuevaEmpresa);
+        }else{
+            System.out.println("Ingrese correctamente todos los datos para guardar una nueva Empresa");
+        }
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+        txt_tipo.setItems(txt_tipo_list);
+        txt_tipo.setValue("Tipo");
     }
 }
