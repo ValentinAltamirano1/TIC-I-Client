91ca42a5eeffdc818a627087511e42a30b40cd96
ValentinAltamirano1 <valtamirano@correo.um.edu.uy>
2022-10-24 14:55:15 -0300

    cambio2.0

--- RestauranteCliente/src/main/java/com/example/usuario/usuario/Actividades/ActividadesController.java
+++ RestauranteCliente/src/main/java/com/example/usuario/usuario/Actividades/ActividadesController.java
@@ -1,18 +1,32 @@
 package com.example.usuario.usuario.Actividades;
 
+import com.example.usuario.usuario.Application;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+import kong.unirest.GetRequest;
+import kong.unirest.Unirest;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
 
-public class ActividadesController {
+public class ActividadesController implements Initializable {
     Scene scene;
 
     Stage stage;
@@ -47,46 +61,51 @@ public class ActividadesController {
     private Button cerrar_sesion;
 
     @FXML
-    void CanchasClickedButton(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("/com/example/usuario/usuario/Actividades/TiposActividades/Canchas-view.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
+    private GridPane grid;
+
+    List<Actividades> actividades= new ArrayList<>();
+
+    private List<Actividades> getData() {
+        List<Actividades> actividadesList =null;
+        try{
+            GetRequest apiResponse = Unirest.get("http://localhost:8080/api/v1/gimnasio/actividades")
+                    .header("Content-Type", "application/json");
+            String temp = apiResponse.asJson().getBody().toString();
+            ObjectMapper mapper = new ObjectMapper();
+            actividadesList = mapper.readValue(temp, new TypeReference<List<Actividades>>(){});
+            }
+        catch (Exception ignored){}
+        return actividadesList;
     }
 
-    @FXML
-    void ExteriorClickedButton(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("/com/example/usuario/usuario/Actividades/TiposActividades/Exterior-view.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
-    }
-    @FXML
-    void CerrarSesionClickedButton(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("/com/example/usuario/usuario/LogIn-view.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
-    }
-    @FXML
-    void GimnasiosClickedButton(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("/com/example/usuario/usuario/Actividades/TiposActividades/Gimnasios-view.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
-    }
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        actividades.addAll(getData());
+        System.out.println(getData());
+         int row=1;
+         int colum =0;
+         try {
+             for (int i = 0; i < actividades.size(); i++) {
+                 FXMLLoader fxmlLoader = new FXMLLoader();
+                 fxmlLoader.setLocation(getClass().getResource("com.example.usuario.usuario.Actividades.Desplegar.fxml"));
+                 AnchorPane anchorPane = fxmlLoader.load();
 
-    @FXML
-    void NauticaClickedButton(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("/com/example/usuario/usuario/Actividades/TiposActividades/Nautico-view.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
-    }
 
+                 DesplegarController desplegarController = fxmlLoader.getController();
+                 desplegarController.setData(actividades.get(i));
+
+                 if (colum == 1) {
+                     colum = 0;
+                     row++;
+                 }
+
+                 grid.add(anchorPane, colum++, row);
+                 GridPane.setMargin(anchorPane,new Insets(10));
+
+
+             }
+         }catch (Exception ignored){
+
+         }
+    }
 }
--- RestauranteCliente/src/main/java/com/example/usuario/usuario/Actividades/DesplegarController.java
+++ RestauranteCliente/src/main/java/com/example/usuario/usuario/Actividades/DesplegarController.java
@@ -4,6 +4,8 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 
+import javax.swing.text.html.ImageView;
+import java.awt.*;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -21,8 +23,21 @@ public class DesplegarController implements Initializable {
     @FXML
     private Label precio1;
 
+    @FXML
+    private ImageView imageView;
+
+    private Actividades actividades;
+
+    public void setData(Actividades actividades){
+        System.out.println("HOLA!");
+        this.actividades = actividades;
+        nombre1.setText(actividades.getNombre());
+        precio1.setText(String.valueOf(actividades.getPrecio()));
+        horario1.setText(actividades.getHorario());
+        capacidad1.setText(String.valueOf(actividades.getCapacidad()));
+    }
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
     }
 }
