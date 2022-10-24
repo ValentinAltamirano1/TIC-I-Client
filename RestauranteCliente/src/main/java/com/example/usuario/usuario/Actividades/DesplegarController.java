2e35bc0a16373b093025b123280b9dc1861fb3c7
valemailhos <vmailhos@correo.um.edu.uy>
2022-10-24 12:31:07 -0300

    .

---
+++ RestauranteCliente/src/main/java/com/example/usuario/usuario/Actividades/DesplegarController.java
@@ -0,0 +1,28 @@
+package com.example.usuario.usuario.Actividades;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class DesplegarController implements Initializable {
+
+    @FXML
+    private Label capacidad1;
+
+    @FXML
+    private Label horario1;
+
+    @FXML
+    private Label nombre1;
+
+    @FXML
+    private Label precio1;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
