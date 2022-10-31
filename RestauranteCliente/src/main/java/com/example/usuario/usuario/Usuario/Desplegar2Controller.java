9a78d43b52ab15c7335ef845ca484263fe79296d
valemailhos <vmailhos@correo.um.edu.uy>
2022-10-31 17:01:44 -0300

    Cambio cosas de vista

---
+++ RestauranteCliente/src/main/java/com/example/usuario/usuario/Usuario/Desplegar2Controller.java
@@ -0,0 +1,16 @@
+package com.example.usuario.usuario.Usuario;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+public class Desplegar2Controller {
+
+    @FXML
+    private ImageView foto_usuario;
+
+    @FXML
+    private Label nombre;
+
+}
+
