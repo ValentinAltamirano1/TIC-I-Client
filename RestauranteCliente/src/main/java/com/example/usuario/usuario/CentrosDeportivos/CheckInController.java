e6fab62dc9103f828de75dd98b8902b1f70dff4a
valemailhos <vmailhos@correo.um.edu.uy>
2022-11-01 16:16:17 -0300

    Agrego cosas del check in

--- RestauranteCliente/src/main/java/com/example/usuario/usuario/CentrosDeportivos/CheckInController.java
+++ RestauranteCliente/src/main/java/com/example/usuario/usuario/CentrosDeportivos/CheckInController.java
@@ -1,17 +1,59 @@
 package com.example.usuario.usuario.CentrosDeportivos;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class CheckInController {
-    public void CerrarSesionClickedButton(ActionEvent actionEvent) {
+
+    Stage stage;
+    Scene scene;
+
+    @FXML
+    void CreadosClickedButton(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/com/example/usuario/usuario/CentrosDeportivos/TablaCentrosDeportivos-view.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 
-    public void CreadosClickedButton(ActionEvent actionEvent) {
+    @FXML
+    void CerrarSesionClickedButton(ActionEvent event)throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/com/example/usuario/usuario/LogIn-view.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 
-    public void VolverClickedButton(ActionEvent actionEvent) {
+    @FXML
+    void VolverClickedButton(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/com/example/usuario/usuario/Opciones1-view.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 
     public void ConfirmarClickedButton(ActionEvent actionEvent) {
     }
+
+    @FXML
+    private GridPane grid;
+
+    @FXML
+    private Label label;
+
+    @FXML
+    private ScrollPane scroll;
 }
---
+++ RestauranteCliente/src/main/java/com/example/usuario/usuario/CentrosDeportivos/Desplegar3Controller.java
@@ -0,0 +1,48 @@
+package com.example.usuario.usuario.CentrosDeportivos;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class Desplegar3Controller {
+
+    @FXML
+    private Label capacidad1;
+
+    @FXML
+    private Button confirmar_button;
+
+    @FXML
+    private Label horario1;
+
+    @FXML
+    private Label nombre1;
+
+    @FXML
+    private Label pasaporte;
+
+    @FXML
+    private Label precio1;
+
+    @FXML
+    private Label titulo_capacidad;
+
+    @FXML
+    private Label titulo_horario;
+
+    @FXML
+    private Label titulo_nombre;
+
+    @FXML
+    private Label titulo_pasaporte;
+
+    @FXML
+    private Label titulo_precio;
+
+    @FXML
+    void ConfirmarClickedButton(ActionEvent event) {
+
+    }
+
+}
