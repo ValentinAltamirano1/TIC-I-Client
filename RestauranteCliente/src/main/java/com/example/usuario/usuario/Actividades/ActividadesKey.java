2aaaaccadb0c352a5245baf2b2d3fba1e07fd1b5
ValentinAltamirano1 <valtamirano@correo.um.edu.uy>
2022-10-31 23:59:50 -0300

    arreglo parcial de actividades, se guardan pero no se listan

---
+++ RestauranteCliente/src/main/java/com/example/usuario/usuario/Actividades/ActividadesKey.java
@@ -0,0 +1,69 @@
+package com.example.usuario.usuario.Actividades;
+
+import com.example.usuario.usuario.CentrosDeportivos.CentroDeportivo;
+
+import java.io.Serializable;
+
+public class ActividadesKey  {
+
+    private CentroDeportivo centrosDeportivos;
+
+    private String nombre;
+
+    private String horario;
+
+    private String fecha;
+
+
+    public ActividadesKey() {
+    }
+
+    public ActividadesKey(CentroDeportivo centrosDeportivos, String nombre, String horario, String fecha) {
+        this.centrosDeportivos = centrosDeportivos;
+        this.nombre = nombre;
+        this.horario = horario;
+        this.fecha = fecha;
+    }
+
+    public CentroDeportivo getCentrosDeportivos() {
+        return centrosDeportivos;
+    }
+
+    public void setCentrosDeportivos(CentroDeportivo centrosDeportivos) {
+        this.centrosDeportivos = centrosDeportivos;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getHorario() {
+        return horario;
+    }
+
+    public void setHorario(String horario) {
+        this.horario = horario;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    @Override
+    public String toString() {
+        return "ActividadesKey{" +
+                "centrosDeportivos=" + centrosDeportivos +
+                ", nombre='" + nombre + '\'' +
+                ", horario='" + horario + '\'' +
+                ", fecha='" + fecha + '\'' +
+                '}';
+    }
+}
